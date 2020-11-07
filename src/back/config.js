6d364f362e02ed2a8216a4b005e695393a174b41
raul <garciadelafuenteraul@gmail.com>
2020-11-07 17:23:20 +0100

    update package

--- src/back/config.js
+++ src/back/config.js
@@ -3,8 +3,15 @@ const Const = {
         host: '127.0.0.1',
         port: '8989',
         user: 'root',
-        password: 'root',
-        database: 'storagecontrol_test'
+		password: 'root',
+		database: 'storagecontrol_test'
     }
+   /*  DB: {
+        host: '192.168.2.167',
+        port: '3306',
+        user: 'storage',
+        password: 'storage',
+        database: 'storagecontrol_test'
+    } */
 }
 module.exports = Const
